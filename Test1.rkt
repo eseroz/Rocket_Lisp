abaa52a66f218f93f72d3aefed926bd5eaa4dbb1
Eser Öznohut <esero@outlook.com.tr>
2018-03-25 19:36:50 +0300

    Capter 1

--- Test1.rkt
+++ Test1.rkt
@@ -3,3 +3,23 @@
 (circle 30)
 (rectangle 10 20)
 )
+
+(define c (circle 10))
+c
+
+(define x 100)
+(* x 5)
+
+(define (four p)
+  (define two-p (hc-append p p))
+  (vc-append two-p two-p))
+> (four (circle 10))
+
+(define (topla a b) (+ a b))
+(topla 10 20)
+
+
+(define (append-four rec)
+(define two-rec (hc-append rec rec))
+  (vc-append two-rec two-rec))
+(append-four (rectangle 10 20))
